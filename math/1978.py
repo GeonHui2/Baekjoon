b2bda4292598fb68aa2f6d3d192a34b9054053f4
GeonHui2 <92250144+GeonHui2@users.noreply.github.com>
2022-04-26 00:18:05 +0900

    Create 1978.py

---
+++ math/1978.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+num = list(map(int, input().split()))
+
+result = 0
+
+for i in num:
+    cnt = 0
+    if i == 1:
+        continue
+    for j in range(2, i + 1):
+        if i % j == 0:
+            cnt += 1
+    if cnt == 1:
+        result += 1
+
+print(result)
