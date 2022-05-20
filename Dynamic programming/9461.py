ec0fd23469c36792a26ee8d9f6da4f85cf2461ae
GeonHui Lee <022686s@gmail.com>
2022-05-20 20:30:59 +0900

    Dynamic programming

--- Dynamic programming/9461.py
+++ Dynamic programming/9461.py
@@ -1,4 +1,16 @@
 import sys
 
 input = sys.stdin.readline
-asdasd
+
+t = int(input())
+
+dp = [0] * 101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+
+for _ in range(t):
+    n = int(input())
+    for i in range(4, n + 1): # 점화식을 이용하여 피보나치와 같은 원리로 계산한다.
+        dp[i] = dp[i -2] + dp[i -3]
+    print(dp[n])
