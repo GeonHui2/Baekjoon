b01f14a9f2e08bd74d7896e97ae2a318095407e4
GeonHui2 <92250144+GeonHui2@users.noreply.github.com>
2022-06-10 18:59:49 +0900

    Update 2156.py

--- Dynamic programming/2156.py
+++ Dynamic programming/2156.py
@@ -2,15 +2,16 @@ import sys
 
 n = int(sys.stdin.readline())
 
-power = []
-for _ in range(n):
-    power.append(int(sys.stdin.readline()))
+power = [0] * 10001
 
-dp = [0] * (n + 1)
-dp[1] = power[0]
-dp[2] = power[0] + power[1]
+for i in range(1, n + 1):
+    power[i] = int(sys.stdin.readline())
+
+dp = [0] * 10001
+dp[1] = power[1]
+dp[2] = power[1] + power[2]
 
 for i in range(3, n + 1):
-    dp[i] = max(dp[i - 1], dp[i - 3] + power[i - 2] + power[i - 1], dp[i - 2] + power[i  -1])
+    dp[i] = max(dp[i - 1], dp[i - 3] + power[i - 1] + power[i], dp[i - 2] + power[i])
 
 print(dp[n])
