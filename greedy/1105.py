2c889c8ae1e855d20c777c8f5fdd192b4160f945
GeonHui2 <92250144+GeonHui2@users.noreply.github.com>
2022-06-16 16:19:30 +0900

    Create 1105.py

---
+++ greedy/1105.py
@@ -0,0 +1,17 @@
+import sys
+
+l, r = sys.stdin.readline().split()
+
+answer = 0
+
+if len(l) != len(r):
+    print(0)
+else:
+    for i in range(len(l)):
+        if l[i] == r[i]:
+            if l[i] == '8':
+                answer += 1
+        else:
+            break
+
+    print(answer)
